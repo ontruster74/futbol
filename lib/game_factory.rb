d7807ccc25f87333d84e57fbc99dca163ab949ca
ontruster74 <ontruster74@gmail.com>
2025-01-16 10:53:22 -0700

    add missing attr_reader methods

--- lib/game_factory.rb
+++ lib/game_factory.rb
@@ -2,7 +2,7 @@ class GameFactory
   def self.create_games(filepath)
     games = []
     CSV.foreach(filepath, headers: true, header_converters: :symbol) do |row|
-      games << Game.new(row[:home_team_id], row[:away_goals].to_i, row[:home_goals].to_i, row[:venue], row[:venue_link])
+      games << Game.new(row[:game_id], row[:season], row[:type], row[:date_time], row[:away_team_id], row[:home_team_id], row[:away_goals].to_i, row[:home_goals].to_i, row[:venue], row[:venue_link])
     end
     return games
   end
